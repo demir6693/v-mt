1a7dcff754cff4a15c6b37a656323d42004d30a7
demir6693 <demir_agovic@live.com>
2019-02-23 17:13:55 +0100

    responsive for mobile

--- src/components/proizvodResponse/proizvodMobile.vue
+++ src/components/proizvodResponse/proizvodMobile.vue
@@ -1,5 +1,114 @@
 <template>
-    <div>
-        <h1>mobile</h1>
+    <div class="container">
+       
+        <hr>
+        <h3 class="naslov-item">SAMSUNG Galaxy Note9 Dual Sim (Lavender Purple) 6.4", Octa Core, 6 GB, 12.0 Mpix + 12.0 Mpix</h3>
+        
+        <div class="row h-100 justify-content-center align-items-center">
+            <img src="https://www.gigatron.rs/img/products/medium/image5b6c350e3b102.png" alt="">
+        </div>
+        
+        <div class="row">
+            <div class="col-3">
+                <img src="https://www.gigatron.rs/img/products/medium/image5b6c350e3b102.png" alt="..." class="img-thumbnail">
+            </div>
+            
+            <div class="col-3">
+                <img src="https://www.gigatron.rs/img/products/medium/image5b6c350e3b102.png" alt="..." class="img-thumbnail">
+            </div>
+            
+            <div class="col-3">
+                <img src="https://www.gigatron.rs/img/products/medium/image5b6c350e3b102.png" alt="..." class="img-thumbnail">
+            </div>
+            
+            <div class="col-3">
+                <img src="https://www.gigatron.rs/img/products/medium/image5b6c350e3b102.png" alt="..." class="img-thumbnail">
+            </div>
+        </div>
+
+        <hr>
+
+        <h6>Moguća kupovina čekovima građana.</h6>
+
+        <p class="">
+            <i class="fas fa-circle na-stanju"></i>
+            Na stanju
+        </p>
+
+        <div class="row">
+            <div class="col-3">
+                <i class="fas fa-truck fa-2x"></i>
+            </div>
+            <div class="col-9">
+                <strong>Rok isporuke:</strong>
+                2-3 radna dana
+            </div>
+        </div>
+
+        <hr>
+
+        <p>
+            MP 30.990 din.
+        </p>
+        <p class="cena-popust">
+            29.990 din.
+        </p>
+
+        <button type="button" class="btn btn-primary btn-lg btn-block" wfd-id="560">
+            <i class="fas fa-shopping-cart"></i> Dodaj u korpu
+        </button>
+
+        <hr>
+
+        <h4>Specifikacije</h4>
+        <table class="table table-striped">
+            <tbody>
+                <tr>
+                <th scope="row">Dizajn</th>
+                <td>Monoblok</td>
+                </tr>
+                <tr>
+                <th scope="row">Tip ekrana</th>
+                <td>Super AMOLED</td>
+                </tr>
+                <tr>
+                <th scope="row">Ekran osetljiv na dodir</th>
+                <td>Da</td>
+                </tr>
+                <tr>
+                <th scope="row">Broj boja</th>
+                <td>(24-bit) 16.777.216 boja</td>
+                </tr>
+                <tr>
+                <th scope="row">Dijagonala ekrana</th>
+                <td>6.4"</td>
+                </tr>
+                <tr>
+                <th scope="row">Rezolucija</th>
+                <td>2960 x 1440</td>
+                </tr>
+                <tr>
+                <th scope="row">Gustina ekrana</th>
+                <td>~516 ppi</td>
+                </tr>
+                <tr>
+                <th scope="row">Operativni sistem</th>
+                <td>Android 8.1 (Oreo)</td>
+                </tr>
+                <tr>
+                <th scope="row">RAM memorija</th>
+                <td>6 GB</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
+
+
+<style>
+
+.naslov-item{
+    font-size: 18pt;
+}
+
+</style>
