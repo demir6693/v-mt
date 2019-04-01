384bc53adc5420fff247d50a79d4584a1a106502
demir6693 <demir_agovic@live.com>
2019-04-01 20:08:43 +0200

    edit prod page 3/4

--- src/components/admin/lista-proizvoda.vue
+++ src/components/admin/lista-proizvoda.vue
@@ -26,7 +26,7 @@
                         {{items.stanje}}
                     </p>
                     </td>
-                <td><i class="fas fa-edit fa-2x" @click="showModalProduct(items.id)"></i></td>
+                <td><i style="cursor: pointer;" class="fas fa-edit fa-2x" @click="showModalProduct(items.id)"></i></td>
                 </tr>
             </tbody>
         </table>
@@ -36,26 +36,28 @@
                 <h2>Izmeni artikal</h2>
                 <hr>
                 <div class="row">
-                    <div class="col-5">
+                    <div class="col-4">
                         <img v-bind:src="urlPic" alt="" class="titlePicture">
                         <br>
-                        <i class="fas fa-trash-alt fa-2x"></i> | <i class="fas fa-exchange-alt fa-2x"></i>
+                        <i class="fas fa-trash-alt fa-2x picture-icon"></i> | <i class="fas fa-exchange-alt fa-2x picture-icon"></i>
                         <hr>
                         <div class="row">
-                            <div class="col-3" v-for="picture, index in pictureSlide">
+                            <div class="col-3" v-for="picture in pictureSlide">
                                 <img v-bind:src="picture.picture" alt="..." class="img-thumbnail mini-picture">
-                                <i class="fas fa-trash-alt"></i> | <i class="fas fa-exchange-alt"></i>
+                                <i class="fas fa-trash-alt picture-icon" @click="deleteSlidePicture(picture.id)"></i> | <i class="fas fa-exchange-alt picture-icon"></i>
                             </div>
                             
-                            <div class="col-3">
-                                <button type="button" class="btn btn-primary btn-lg btn-block" wfd-id="561">
+                            <div class="col-3 add-picture">
+                                <label for="files" class="btn">
                                     <i class="fas fa-plus fa-3x"></i>
-                                </button>
+                                    <p>Dodaj sliku</p>
+                                </label>
+                                <input id="files" style="visibility:hidden;" type="file" accept="image/*" @change="convertPicToBase64">                               
                             </div>
 
                         </div>
                     </div>
-                    <div class="col-7">
+                    <div class="col-4">
 
                         <div class="form-group" wfd-id="318">
                             <label class="col-form-label" for="inputDefault" wfd-id="319">Naziv artikla</label>
@@ -85,6 +87,51 @@
                             Izmeni
                         </button>
                     </div>
+
+                    <div class="col-4">
+                        <h4>Specifikacije</h4>
+
+                        <table class="table table-hover slide-table">
+                            <thead>
+                                <tr class="table-active">
+                                <th scope="col">Specifikacija</th>
+                                <th scope="col">Opis spec.</th>
+                                <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="specs in specsProd">
+                                <td>
+                                    <input type="text" class="form-control" id="inputDefault1" v-model="specs.descriptionName">
+                                </td>
+                                <td>
+                                    <input type="text" class="form-control" id="inputDefault2" v-model="specs.description">
+                                </td>
+                                <td>
+                                    <button class="btn btn-primary btn-sm" @click="editSpecs(specs.id, specs.descriptionName, specs.description)"><i class="fas fa-upload"></i></button>
+                                </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <h5>Dodaj Specifikaciju</h5>
+                        <table>
+                            <tbody>
+                                <tr>
+                                <td>
+                                    <input type="text" class="form-control" id="inputDefault1" v-model="addSpecs.descriptionName">
+                                </td>
+                                <td>
+                                    <input type="text" class="form-control" id="inputDefault2" v-model="addSpecs.description">
+                                </td>
+                                <td>
+                                    <button class="btn btn-primary btn-sm" @click="addSpecsProd">
+                                        <i class="fas fa-plus"></i>
+                                    </button>
+                                </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
                 <br>
             </div>
@@ -101,7 +148,12 @@ export default {
             productItems: {},
             productItemsForChange: {},
             urlPic: '',
-            pictureSlide: {}
+            pictureSlide: {},
+            specsProd: {},
+            addSpecs: {
+                descriptionName: '',
+                description: ''
+            }
         }
     },
 
@@ -137,6 +189,7 @@ export default {
            .then(response => {
                this.productItemsForChange = response.body;
                this.decompressImg();
+               this.getSpecsofProd(id);
                this.$modal.show('product');
                this.getPictureSlide(id);
            }, error => {
@@ -164,7 +217,95 @@ export default {
             this.pictureSlide.forEach(element => {
                 element.picture = LzString.decompressFromUTF16(element.picture);
             });
+        },
+
+        convertPicToBase64: function(picture){
+            var input = event.target;
+         
+            if (input.files && input.files[0]) {
+              
+                var reader = new FileReader();
+      
+                reader.onload = (e) => {
+          
+                    var compressImg = LzString.compressToUTF16(e.target.result);
+
+                    var productImage = {
+                        idProd: this.productItemsForChange.id,
+                        picture: compressImg  
+                    }; 
+                        
+                    this.uploadImage(productImage);
+                } 
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        },
+
+        uploadImage: function(picture){
+            this.$http.post("http://localhost:5000/api/productpictures/", picture)
+            .then(response => {
+                this.getPictureSlide(picture.idProd);
+            }, error => {
+                console.log(error);
+            });
+        },
+
+        deleteSlidePicture: function(id)
+        {
+            this.$http.delete("http://localhost:5000/api/productpictures/" + id)
+            .then(response => {
+                this.getPictureSlide(this.productItemsForChange.id);
+            }, error => {
+                console.log(error);
+            });
+        },
+
+        getSpecsofProd: function(id)
+        {
+            this.$http.get("http://localhost:5000/api/productdescriptions/" + id)
+            .then(response => {
+                this.specsProd = response.body;
+            }, error => {
+                console.log(error);
+            })
+        },
+
+        editSpecs: function(id, specs, value)
+        {
+            var specsofProd = {
+                id: id,
+                productId: this.productItemsForChange.id,
+                descriptionName: specs,
+                description: value
+            };
+
+            this.$http.put("http://localhost:5000/api/productdescriptions/" + id, specsofProd)
+            .then(response => {
+                this.getSpecsofProd(this.productItemsForChange.id);
+            }, error => {
+                console.log(error);
+            });
+        },
+
+        addSpecsProd: function()
+        {   
+            var specsofProd = {
+                productId: this.productItemsForChange.id,
+                descriptionName: this.addSpecs.descriptionName,
+                description: this.addSpecs.description
+            };
+
+            this.$http.post("http://localhost:5000/api/productdescriptions/", specsofProd)
+            .then(response => {
+                this.getSpecsofProd(this.productItemsForChange.id);
+                this.addSpecs.descriptionName = '';
+                this.addSpecs.description = '';
+            }, error => {
+                console.log(error);
+            });
         }
+        
     }
 }
 </script>
@@ -181,6 +322,17 @@ export default {
 }
 
 .add-picture{
- 
+    text-align: center;
+    cursor: pointer;
+}
+
+.picture-icon{
+    cursor: pointer;
+}
+
+.slide-table {
+  max-height: 450px;
+  overflow: auto;
+  display:inline-block;
 }
 </style>
